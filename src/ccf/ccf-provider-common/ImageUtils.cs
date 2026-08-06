7ac61bb453907fa14fa390c8f7296c4eb118a580
Anant <39825466+anantshankar17@users.noreply.github.com>
2026-08-06 17:41:51 +0530

    Bumping version for 9.0.0 release (#106)
    * Bumping version for 9.0.0 release
    
    * Fix incorrect path issue in release

--- src/ccf/ccf-provider-common/ImageUtils.cs
+++ src/ccf/ccf-provider-common/ImageUtils.cs
@@ -10,7 +10,7 @@ namespace CcfProvider;
 public static class ImageUtils
 {
     private const string McrRegistryUrl = "mcr.microsoft.com/azurecleanroom";
-    private const string McrTag = "8.0.0";
+    private const string McrTag = "9.0.0";
 
     private static SemaphoreSlim semaphore = new(1, 1);
 
--- src/cleanroom-cluster/cleanroom-cluster-provider-common/ImageUtils.cs
+++ src/cleanroom-cluster/cleanroom-cluster-provider-common/ImageUtils.cs
@@ -11,7 +11,7 @@ namespace CleanRoomProvider;
 public static class ImageUtils
 {
     private const string McrRegistryUrl = "mcr.microsoft.com/azurecleanroom";
-    private const string McrTag = "8.0.0";
+    private const string McrTag = "9.0.0";
 
     private static SemaphoreSlim semaphore = new(1, 1);
 
